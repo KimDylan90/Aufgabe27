5892e9628ff6bb46d7be45061bc74633971f9e1c
Kim Dylan <kim.wuebbena@docc.techstarter.de>
2023-03-17 13:43:55 +0100

    aufgabe2 added

---
+++ aufgabe2.py
@@ -0,0 +1,7 @@
+print("STUNDENRECHNER")
+print("==================")
+stunden = int(input("Gib eine Zahl ein (Stunden): "))
+minuten = stunden * 60
+sekunden = stunden * 60 * 60
+print("Das sind", minuten, "Minuten und", sekunden, "Sekunden!")
+print("WOW!")
